c954557abd425ad6af545ca3e6712f2541141aae
Ajay <ajaykr00kj@gmail.com>
2021-05-22 12:12:04 +0530

    FyleFrontend

---
+++ src/constants.js
@@ -0,0 +1,12 @@
+export const options = [
+  { label: "Mumbai", value: "MUMBAI" },
+  { label: "Hyderabad", value: "HYDERABAD" },
+  { label: "Chennai", value: "CHENNAI" },
+  { label: "Bangalore", value: "BANGALORE" },
+  { label: "Haryana", value: "haryana" },  
+  { label: "Akola", value: "AKOLA" },
+  { label: "Kolkata", value: "Kolkata" },
+  { label: "UttarPradesh", value: "Uttar" }
+];
+
+export const defaultDropdownValue = { label: "Mumbai", value: "MUMBAI" };
